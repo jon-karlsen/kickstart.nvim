09fe0a47d86477daf62aa214f78470510510bdd4
Jon Karlsen <karlsen.jon@icloud.com>
2025-05-18 17:36:28 +0200

    laravel

--- init.lua
+++ init.lua
@@ -237,6 +237,24 @@ vim.filetype.add({ extension = { templ = "templ" } })
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup {
   {
+    "adalessa/laravel.nvim",
+    dependencies = {
+      "tpope/vim-dotenv",
+      "nvim-telescope/telescope.nvim",
+      "MunifTanjim/nui.nvim",
+      "kevinhwang91/promise-async",
+    },
+    cmd = { "Laravel" },
+    keys = {
+      { "<leader>la", ":Laravel artisan<cr>" },
+      { "<leader>lr", ":Laravel routes<cr>" },
+      { "<leader>lm", ":Laravel related<cr>" },
+    },
+    event = { "VeryLazy" },
+    opts = {},
+    config = true,
+  },
+  {
     "nvim-neotest/neotest",
     dependencies = {
       "nvim-neotest/nvim-nio",
